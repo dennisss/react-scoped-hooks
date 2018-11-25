a3551e35f2ed2f8dbfc7d112b60c5d2f0c41260c
Dennis Shtatnov <densht@gmail.com>
2018-11-25 09:26:35 -0500

    Add a typescript entry point

---
+++ dist/index.ts
@@ -0,0 +1,8 @@
+/*
+	NOTE: If packing this for npm, this file should be deleted as it will be replaced by the regular index upon build
+
+	This just makes it a little easier to use this as a package referenced by the git repo
+*/
+
+export * from '../src/index';
+
